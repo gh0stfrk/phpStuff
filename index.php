aaf742f1081b7a00d329ab9f7bad1d19de79d742
gh0stfrk <salmansyyd12@gmail.com>
2024-06-28 11:31:47 +0530

    echos and comments

--- index.php
+++ index.php
@@ -2,4 +2,26 @@
 
 echo "Hello world";
 
+echo "gh0stfrk";
+echo "<br>";
+echo "screaming in the dark";
+
+// Multible echo's in a single line
+// Case Insensitive echos's
+echo "Hello"; eCho "what are you doing ?"; ECHO "this is lame";
+
+// to create a line break use <br/> tag in between 
+echo "<br/>";
+echo "<br/>";
+echo "This is a line <br/>";
+echo "This is another line </br>";
+
+// Single line comment
+# Single line comment 
+/*
+  Multiline comment
+  .
+  . 
+*/
+
 ?>
---
+++ touch.php
@@ -0,0 +1,4 @@
+<?php
+
+header("Conent-Type: application/json");
+?>
