cf43242aeeba8460bbaef7727fc099bf647dd139
gh0stfrk <salmansyyd12@gmail.com>
2024-06-28 17:37:38 +0530

    operators in php

---
+++ op.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Operators in PHP</h1>
+    <?php
+    $a = 10;
+    $b = 2;
+    echo "a = $a <br/> b = $b";
+    echo "<br/>";
+    echo "<br/>Addition<br/>";
+    echo "$a + $b = " . $a + $b;
+    echo "<br/>Subtraction <br/>";
+    echo "$a - $b = ". $a - $b;
+    echo "<br/> Multiplication </br>";
+    echo "$a * $b = ". $a * $b;
+    echo "<br/> Division </br>";
+    echo "$a / $b = ". $a / $b;
+    echo "<br/> Modulus </br>";
+    echo "$a % $b = ". $a % $b;
+    echo "<br/> Exponential </br>";
+    echo "$a ** $b = ". $a ** $b;
+    ?>
+</body>
+
+</html>
